496495f75c2920b5df4865e9fda097645de96341
louis7552912 <louis@movegpt.io>
2024-12-03 11:21:23 +0700

    update og

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,8 +18,8 @@ export default function Document() {
         {/*<meta property="og:title" content="AI-based Blockchain Messenger" />*/}
 
         <meta property="og:image" content="https://staking.unicelab.io/thumbnail.png" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
+        {/*<meta property="og:image:width" content="1200" />*/}
+        {/*<meta property="og:image:height" content="630" />*/}
         {/*<meta name="og:image:alt" content={'AI-based Blockchain Messenger'} />*/}
         <meta name="og:url" content="" />
 
